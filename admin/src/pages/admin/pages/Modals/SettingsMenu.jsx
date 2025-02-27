ef0af5b7239b2c869994ef268c7ad363c5f7fd07
ShalakaGajanayaka <shalakagajanayake123@gmail.com>
2025-02-27 12:28:10 +0530

    Revert "settingsMenu - If you are not logged in as a superadmin, turn off the remove menuitem."
    This reverts commit 6f1f0a56a6354e38c01c0840982234628a03ae11.

--- admin/src/pages/admin/pages/Modals/SettingsMenu.jsx
+++ admin/src/pages/admin/pages/Modals/SettingsMenu.jsx
@@ -6,15 +6,12 @@ import EditUser from './EditUser';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../../../utils/axiosInstance';
 import EditPermitions from './EditPermitions';
-import { useAuth } from '../../../../context/AuthContext';
 
 export default function SettingsMenu({ user }) {
     const [isDemoCreateModalOpen, setIsDemoCreateModalOpen] = useState(false);
     const [isTieModalOpen, setIsTieModalOpen] = useState(false);
     const [isEditUserModalOpen, setIsEditUserModalOpen] = useState(false);
     const [isEditPermitionModalOpen, setIsEditPermitionModalOpen] = useState(false);
-
-    const { user: loggedInUser } = useAuth();
     const navigate = useNavigate();
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
@@ -33,27 +30,18 @@ export default function SettingsMenu({ user }) {
     };
 
     const handleRemove = async () => {
-        if (loggedInUser.role !== 'superadmin') {
-            
-            setError('You do not have permission to remove this user.');
-            return;
-        }
-        else {
-            try {
-                const response = await axiosInstance.delete(`/api/users/${user._id}`);
-                if (response.status === 200) {
-                    setSuccess('User removed successfully!');
-                    // Optionally, you can refresh the page or update the state to reflect the removal
-                    window.location.reload();
-                } else {
-                    throw new Error(response.data.error || 'Something went wrong');
-                }
-            } catch (err) {
-                setError(err.message);
+        try {
+            const response = await axiosInstance.delete(`/api/users/${user._id}`);
+            if (response.status === 200) {
+                setSuccess('User removed successfully!');
+                // Optionally, you can refresh the page or update the state to reflect the removal
+                window.location.reload();
+            } else {
+                throw new Error(response.data.error || 'Something went wrong');
             }
+        } catch (err) {
+            setError(err.message);
         }
-
-
     };
 
     return (
