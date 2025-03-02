17431bfbb154a1ac5002a4753593977394b426e2
ShalakaGajanayake <shalakagajanayake123@gmail.com>
2025-03-02 12:31:22 +0530

    userTask added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,6 +8,7 @@ import AdminLayout from "./pages/admin/layout/AdminLayout";
 import UserList from "./pages/admin/pages/UserList";
 import AdminList from "./pages/admin/pages/AdminList";
 import TieUser from "./pages/admin/pages/TieUser";
+import UserTasks from "./pages/admin/pages/UserTasks";
 import AddUser from "./pages/admin/pages/AddUser";
 import AddAdmin from "./pages/admin/pages/AddAdmin";
 import Withdrawels from "./pages/admin/pages/Withdrawels";
@@ -27,6 +28,7 @@ const App = () => {
             <Route path="/admin/add-user" element={<AddUser />} />
             <Route path="/admin/user-list" element={<UserList />} />
             <Route path="/admin/user-tie/:id" element={<TieUser />} />
+            <Route path="/admin/user-tasks/:id" element={<UserTasks />} />
             <Route path="/admin/add-admin" element={<AddAdmin />} />
             <Route path="/admin/admin-list" element={<AdminList />} />
             <Route path="/admin/withdrawels/:id" element={<Withdrawels />} />
