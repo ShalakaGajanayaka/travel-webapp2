317fdc5f32bfb9da3246979988cb5982b72e778d
ShalakaGajanayake <shalakagajanayake123@gmail.com>
2025-03-19 20:26:23 +0530

    SettingsMenu.jsx - when called tie remove the #tag

--- admin/src/pages/admin/pages/Modals/SettingsMenu.jsx
+++ admin/src/pages/admin/pages/Modals/SettingsMenu.jsx
@@ -68,7 +68,7 @@ export default function SettingsMenu({ user }) {
                                 <a
                                     onClick={() => navigate(`/admin/user-tie/${user._id}`, { state: { user } })}
                                     className={`block px-4 py-2 text-sm text-gray-700 ${active ? 'bg-gray-100' : ''}`}
-                                    href="#"
+                                    href=""
                                 >
                                     Tie
                                 </a>
