0aa1eb4786df9dbce71339fd48fdcc51ace7f913
ShalakaGajanayake <shalakagajanayake123@gmail.com>
2025-04-20 08:02:31 +0530

    topnova image updated

--- client/src/pages/landing/TopNav.jsx
+++ client/src/pages/landing/TopNav.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/images/intrepid-logo.svg';
-import logoSmall from '../../assets/images/intrepid-logo-small.svg';
+import logoSmall from '../../assets/images/intrepid-logo.svg';
 
 export default function TopNav() {
   const navigate = useNavigate();
